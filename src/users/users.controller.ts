8ffd67e6911cf56864e1ec1e0214570a7b2164cd
Jacob Andersen <jacob@algorithmjunkie.com>
2023-09-14 12:39:07 +0800

    forgot return types on user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,31 +6,32 @@ import { HasPermissions } from 'src/permission/permissions.decorator';
 import { Permission } from 'src/permission/permission.class';
 import { Request, Response } from 'express';
 import { IsAuthenticatedGuard } from 'src/auth/is-authenticated.guard';
+import { IUser } from 'src/shared-types/user/user.interface';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto): Promise<void> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
   @UseGuards(IsAuthenticatedGuard)
-  findSelf(@Req() req: Request) {
+  findSelf(@Req() req: Request): Promise<IUser> {
     return this.usersService.findOneById(req.user.id);
   }
 
   @Patch()
   @HasPermissions(Permission.ChangeUserDetails)
-  updateSelf(@Req() req: Request, @Body() updateUserDto: UpdateUserDto) {
+  updateSelf(@Req() req: Request, @Body() updateUserDto: UpdateUserDto): Promise<void> {
     return this.usersService.update(req.user.id, updateUserDto);
   }
 
   @Delete()
   @UseGuards(IsAuthenticatedGuard)
-  removeSelf(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+  removeSelf(@Req() req: Request, @Res({ passthrough: true }) res: Response): void {
     this.usersService.remove(req.user.id);
     res.clearCookie('refresh');
   }
